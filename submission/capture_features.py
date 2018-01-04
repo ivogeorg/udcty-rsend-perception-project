d4571b8502cee2b637ab404d1ca2e822f97c57cd
Ivo Georgiev <ivogeorg@gmail.com>
2018-01-04 07:47:02 -0800

    features & training from sensor_stick

---
+++ submission/capture_features.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+import numpy as np
+import pickle
+import rospy
+
+from sensor_stick.pcl_helper import *
+from sensor_stick.training_helper import spawn_model
+from sensor_stick.training_helper import delete_model
+from sensor_stick.training_helper import initial_setup
+from sensor_stick.training_helper import capture_sample
+from sensor_stick.features import compute_color_histograms
+from sensor_stick.features import compute_normal_histograms
+from sensor_stick.srv import GetNormals
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import PointCloud2
+
+
+def get_normals(cloud):
+    get_normals_prox = rospy.ServiceProxy('/feature_extractor/get_normals', GetNormals)
+    return get_normals_prox(cloud).cluster
+
+
+if __name__ == '__main__':
+    rospy.init_node('capture_node')
+
+    # Perception Exercise 3
+    models0 = [\
+       'beer',
+       'bowl',
+       'create',
+       'disk_part',
+       'hammer',
+       'plastic_cup',
+       'soda_can']
+
+    # Perception Project - Pick List 1
+    models = [\
+       'biscuits',
+       'soap',
+       'soap2']
+
+    # Perception Project - Pick List 2
+    models2 = [\
+       'biscuits',
+       'book',
+       'glue',
+       'soap',
+       'soap2']
+
+    # Perception Project - Pick List 3
+    models3 = [\
+       'biscuits',
+       'book',
+       'eraser',
+       'glue',
+       'soap',
+       'soap2',
+       'sticky_notes',
+       'soda_can']
+
+    # Disable gravity and delete the ground plane
+    initial_setup()
+    labeled_features = []
+
+    for model_name in models:
+        spawn_model(model_name)
+
+        num_attempts = 30 # orig 5
+        for i in range(num_attempts):
+            # make five attempts to get a valid point cloud then give up
+            sample_was_good = False
+            try_count = 0
+            while not sample_was_good and try_count < 5:
+                sample_cloud = capture_sample()
+                sample_cloud_arr = ros_to_pcl(sample_cloud).to_array()
+
+                # Check for invalid clouds.
+                if sample_cloud_arr.shape[0] == 0:
+                    print('Invalid cloud detected')
+                    try_count += 1
+                else:
+                    sample_was_good = True
+
+            # Extract histogram features
+            use_hsv = True
+            chists = compute_color_histograms(sample_cloud, using_hsv=True)
+            normals = get_normals(sample_cloud)
+            nhists = compute_normal_histograms(normals)
+            feature = np.concatenate((chists, nhists))
+            labeled_features.append([feature, model_name])
+
+        delete_model()
+
+
+    pickle.dump(labeled_features, open('training_set.sav', 'wb'))
+
